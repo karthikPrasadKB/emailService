98e957e2a1b46c4fd59bd081b61708268dc26614
Karthik <kb.karthick97@gmail.com>
2024-06-03 13:36:03 +0530

    added google smtp to verify its working

--- src/main/java/com/emailService/emailService/consumers/EmailConsumer.java
+++ src/main/java/com/emailService/emailService/consumers/EmailConsumer.java
@@ -1,11 +1,17 @@
 package com.emailService.emailService.consumers;
 
 import com.emailService.emailService.dtos.EmailDTO;
+import com.emailService.emailService.utils.EmailUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.util.Properties;
 
 @Service
 public class EmailConsumer {
@@ -24,10 +30,30 @@ public class EmailConsumer {
         }catch (JsonProcessingException e){
             throw new RuntimeException("Email could not be converted into emailDTO object");
         }
-        System.out.println("To : " + emailDTO.getTo());
-        System.out.println("From : " + emailDTO.getFrom());
-        System.out.println("Message : " + emailDTO.getMessage());
-        System.out.println("Subject : " + emailDTO.getSubject());
+        this.smtpEmail(emailDTO);
+    }
+
+    private void smtpEmail(EmailDTO emailDTO) {
+        final String fromEmail = "xxxxxxx"; //requires valid gmail id
+        final String password = "xxxxxxx"; // correct password for gmail id
+        final String toEmail = emailDTO.getTo(); // can be any email id
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+        props.put("mail.smtp.port", "587"); //TLS Port
+        props.put("mail.smtp.auth", "true"); //enable authentication
+        props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+        //create Authenticator object to pass in Session.getInstance argument
+        Authenticator auth = new Authenticator() {
+            //override the getPasswordAuthentication method
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, password);
+            }
+        };
+        Session session = Session.getInstance(props, auth);
+
+        EmailUtil.sendEmail(session, toEmail,emailDTO.getSubject(), emailDTO.getMessage());
     }
 
 }
